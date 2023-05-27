40ff06684297bb5ed40c921d920d07697db58b82
OLEH FEDORCHUK <fedorchuk2018@gmail.com>
2023-05-27 21:25:11 +0100

    update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+// import simpleLightbox from 'simplelightbox';
+// import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
+// const ulEl = document.querySelector('.gallery');
 
-console.log(galleryItems);
+// const markup = galleryItems.map(({ preview, original, description }) => {
+//   return `<li class="gallery__item">
+//     <a class="gallery__link" href="${original}">
+//       <img
+//         class="gallery__image"
+//         src="${preview}"
+//         data-source="${original}"
+//         alt="${description}"
+//       />
+//     </a>
+//   </li >`;
+// });
+// // Додавання фрагмента в DOM
+// ulEl.insertAdjacentHTML('afterbegin', markup.join(''));
+// ulEl.addEventListener('click', openModal);
+
+// function openModal(event) {
+//   event.preventDefault();
+//   if (event.target.nodeName !== 'IMG') {
+//     return;
+//   }
+//   const instance = simpleLightbox.create(`
+//       <img src="${event.target.dataset.source}">
+//   `);
+
+//   instance.show();
+// }
+// console.log(galleryItems);
