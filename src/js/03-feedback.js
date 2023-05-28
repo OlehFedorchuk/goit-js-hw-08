336dc6135ad658ce2900b6db10de40c9d7d46463
OLEH FEDORCHUK <fedorchuk2018@gmail.com>
2023-05-28 17:39:45 +0100

    update

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,4 +1,34 @@
-// export const x = 9837;
-// export function sum(a, b) {
-//   return a + b;
-// }
+let formData = {};
+const form = document.querySelector('.feedback-form');
+const btn = document.querySelector('button');
+const LS = localStorage;
+
+form.addEventListener('input', function (event) {
+  formData[event.target.name] = event.target.value;
+  LS.setItem('feedback-form-state', JSON.stringify(formData));
+});
+
+if (LS.getItem('feedback-form-state')) {
+  formData = JSON.parse(LS.getItem('feedback-form-state'));
+  for (let key in formData) {
+    form.elements[key].value = formData[key];
+  }
+}
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, message },
+  } = event.currentTarget;
+
+  if (email.value === '' || message.value === '') {
+    return console.log('Please fill in all the fields!');
+  }
+
+  console.log(`Login: ${email.value}, Password: ${message.value}`);
+  event.currentTarget.reset();
+  localStorage.clear();
+}
